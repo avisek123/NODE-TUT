6af670e9aaff7f17939504b1c1d6717186ef8944
avisek123 <mrperfectavi89@gmail.com>
2022-03-24 01:25:57 +0530

    id pass to params

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const app = express();
 app.get("/", (request, response) => {
   response.send("Hello World");
 });
-app.get("/users", (request, response) => {
-  response.send("Hello World User");
+app.get("/users/:Id?", (request, response) => {
+  console.log(request?.params);
+  response.status(200).send(`User ${request.params.Id}`);
 });
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
