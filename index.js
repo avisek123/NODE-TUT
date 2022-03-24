21a40b4e43cc984269faf152ecc31e4e83b94d78
avisek123 <mrperfectavi89@gmail.com>
2022-03-24 08:19:50 +0530

    hypen in route

--- index.js
+++ index.js
@@ -4,8 +4,16 @@ app.get("/", (request, response) => {
   response.send("Hello World");
 });
 app.get("/users/:Id?", (request, response) => {
-  console.log(request?.params);
-  response.status(200).send(`User ${request.params.Id}`);
+  if (request.params.Id) {
+    response.status(200).send(`User ${request.params.Id}`);
+  } else {
+    response.status(200).send(`User`);
+  }
+});
+app.get("/flights/:from?-:to?", (request, response) => {
+  response
+    .status(200)
+    .send(`Search fro flights ${request.params.from}-${request.params.to}`);
 });
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
