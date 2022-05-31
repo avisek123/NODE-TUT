a629ff3f9a180ea5ef0ad5d9ec4d186ee9ecc078
avisek123 <mrperfectavi89@gmail.com>
2022-06-01 00:00:37 +0530

    express done

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-const http = require("http");
-const server = http
-  .createServer(function (req, res) {
-    res.write("Hello World");
-    res.end();
-  })
-  .listen(3000, () => {
-    console.log("Server is running on port 3000");
-  });
-
-// we use http module to create server
+const express = require("express");
+const app = express();
+app.get("/", (request, response) => {
+  response.send("Hello World");
+});
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
