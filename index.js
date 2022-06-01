1ff23329192f5fa812c05d824dbfd670469cbabc
avisek123 <mrperfectavi89@gmail.com>
2022-06-01 23:25:41 +0530

    express router

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 app.get("/", (request, response) => {
   response.send("Hello World");
 });
+app.get("/home", (request, response) => {
+  response.send("<h1>Hello World</h1>");
+});
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
