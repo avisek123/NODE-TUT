54ea926c277ec1aec336404ae1e2cb0f9a7f56b8
avisek123 <mrperfectavi89@gmail.com>
2022-06-03 00:11:30 +0530

    apply middleware globally

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
+var myLogger = function (req, res, next) {
+  console.log("middleware loggoing");
+  next();
+};
+app.use(myLogger); // middleware
 app.get("/", (request, response) => {
   response.send("Hello World");
 });
-app.get("/home/:id?", (request, response) => {
-  response.send("<h1>Hello World</h1>");
+app.get("/user", (request, response) => {
+  response.send("Hello World");
 });
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
