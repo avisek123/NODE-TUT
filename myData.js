14445184578815fd574dd19ea1b571c32293347b
avisek123 <mrperfectavi89@gmail.com>
2022-06-04 10:20:29 +0530

    minor chnages done

--- myData.js
+++ myData.js
@@ -7,3 +7,21 @@ var employeeScheme = mongoose.Schema({
   salary: Number,
   emailID: String,
 });
+// create model
+var EmployeeModel = mongoose.model("Employee", employeeScheme);
+// create object of the model
+var employeeObject = new EmployeeModel({
+  name: "John",
+  age: 30,
+  salary: 10000,
+  emailID: "",
+});
+// get method
+// employeeObject.get(function(err, data){
+//     if(err){
+//         console.log(err);
+//     }
+//     else{
+//         console.log(data);
+//     }
+// })
