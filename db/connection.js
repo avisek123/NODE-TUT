9fd9b5b396ae4ed6750365ec24b0e89107510a7d
avisek123 <mrperfectavi89@gmail.com>
2022-06-12 12:51:22 +0530

    router folder create and arrange code

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 mongoose
   .connect(
     `mongodb+srv://Mongodb:reXoIJTzp7WhF9rl@cluster0.to29v.mongodb.net/MyFirstDb?retryWrites=true&w=majority`,
-    { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }
+    { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() => {
     console.log("connected to db");
--- index.js
+++ index.js
@@ -1,74 +1,9 @@
 const express = require("express");
 require("./db/connection");
-const fruitDB = require("./models/Fruits");
-const studentsDB = require("./models/Students");
+const studentRouter = require("./routers/studentRouter");
 const app = express();
 app.use(express.json());
-
-app.post("/students", async (req, res) => {
-  try {
-    const newStudent = new studentsDB(req.body);
-    const result = await newStudent.save();
-    res.status(200).send(result);
-  } catch (error) {
-    res.status(400).send(e);
-  }
-});
-// read the students data
-app.get("/students", async (req, res) => {
-  try {
-    const students = await studentsDB.find();
-    res.status(200).send(students);
-  } catch (error) {
-    res.status(400).send(error);
-  }
-});
-// // read individual students data
-// app.get("/students/:id", async (req, res) => {
-//   try {
-//     const student = await studentsDB.findById(req.params.id);
-//     res.status(200).send(student);
-//     if (!student) {
-//       res.status(404).send("Student not found");
-//     } else {
-//       res.status(200).send(student);
-//     }
-//   } catch (error) {
-//     res.status(400).send(error);
-//   }
-// });
-// delete a student
-app.delete("/students/:id", async (req, res) => {
-  try {
-    const student = await studentsDB.findByIdAndDelete(req.params.id);
-    if (!student) {
-      res.status(404).send("Student not found");
-    } else {
-      res.status(200).send(student);
-    }
-  } catch (error) {
-    res.status(400).send(error);
-  }
-});
-// update the students by ID
-app.patch("/students/:id", async (req, res) => {
-  try {
-    const student = await studentsDB.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      {
-        new: true,
-      }
-    );
-    if (!student) {
-      res.status(404).send("Student not found");
-    } else {
-      res.status(200).send(student);
-    }
-  } catch (error) {
-    res.status(400).send(error);
-  }
-});
+app.use(studentRouter);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
