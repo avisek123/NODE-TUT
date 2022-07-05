d2c207cf4e2ff2df30019438bfbf57b2bc149238
avisek123 <mrperfectavi89@gmail.com>
2022-07-06 00:16:09 +0530

    crud operations

--- routers/todo.js
+++ routers/todo.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const todoDB = require("../models/Todo");
+// post todos
 router.post("/todos", async (req, res) => {
   try {
     const newTodo = new todoDB(req.body);
@@ -10,4 +11,32 @@ router.post("/todos", async (req, res) => {
     res.status(400).send(error);
   }
 });
+// get todos
+router.get("/todos", async (req, res) => {
+  try {
+    const result = await todoDB.find();
+    res.status(200).send(result);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+// delete by id
+router.delete("/todos/:id", async (req, res) => {
+  try {
+    const result = await todoDB.findByIdAndDelete(req.params.id);
+    res.status(200).send(result);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+// update by id
+router.put("/todos/:id", async (req, res) => {
+  try {
+    const result = await todoDB.findByIdAndUpdate(req.params.id, req.body);
+    res.status(200).send(result);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
 module.exports = router;
